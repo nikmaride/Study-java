cb804af945df967ad5c5191a6739e59b64abd8de
nikmaride <nikmaden@yandex.ru>
2021-05-21 22:48:04 +0300

    Test push

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        java.lang.Integer x = 1;
+        Integer xInteger = x;
+        xInteger.toString();
+        java.lang.String name = "Marina";
+                System.out.println(name.toUpperCase(java.util.Locale.ROOT));
+    }
+}
